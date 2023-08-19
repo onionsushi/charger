d328679ac31c5bc344f015d8521b1a27f4011b4e
onionsushi <abelismyid@hotmail.com>
2023-08-19 18:06:12 -0400

    bluetooth fix

--- bluetoothControl.py
+++ bluetoothControl.py
@@ -2,11 +2,11 @@ import asyncio
 from bleak import BleakScanner, BleakClient
 
 # Replace with your service UUID and characteristic UUID
-service_uuid = "19B10010-E8F2-537E-4F6C-D104768A1214"
-characteristic_uuid = "19B10011-E8F2-537E-4F6C-D104768A1214"
+service_uuid = "a5d3a518-3d69-11ee-be56-0242ac120002"
+characteristic_uuid = "bd613920-3d64-11ee-be56-0242ac120002"
 
 # Value to write to the characteristic (example: [1, 2, 3, 4, 5])
-value = [1, 2, 3, 4, 5]
+value = "Hello, World!"
 
 async def discover_and_write(service_uuid, characteristic_uuid, value):
     # Scan for nearby devices
@@ -15,19 +15,24 @@ async def discover_and_write(service_uuid, characteristic_uuid, value):
     # Check each device for the specified service UUID
     for device in devices:
         # Connect to the device
-        async with BleakClient(device.address) as client:
-            services = await client.get_services()
-            if any(s.uuid == service_uuid for s in services):
-                print(f"Found device with address {device.address} that provides the service {service_uuid}.")
+        if len(device.metadata.get("uuids", ["0"])) != 0 and device.metadata.get("uuids", ["0"])[0] == service_uuid:
+            try:
+                async with BleakClient(device.address) as client:
+                    print(f"Found device with address {device.address} that provides the service {service_uuid}.")
 
-                # Write a value to the characteristic
-                await client.write_gatt_char(characteristic_uuid, bytearray(value))
-                print(f"Written value '{value}' to characteristic {characteristic_uuid}.")
+                    # Write a value to the characteristic
+                    if type(value) != str:
+                        await client.write_gatt_char(characteristic_uuid, bytearray(value))
+                    else:
+                        await client.write_gatt_char(characteristic_uuid, bytearray(value.encode('utf-8')))
+                    print(f"Written value '{value}' to characteristic {characteristic_uuid}.")
 
-                # Disconnect from the device
-                return
+                    # Disconnect from the device
+                    return
+            except:
+                print("failed to get device info")
 
     print(f"No devices found that provide the service {service_uuid}.")
 
 # Run the discover_and_write function
-asyncio.run(discover_and_write(service_uuid, characteristic_uuid, value))
+asyncio.run(discover_and_write(service_uuid, characteristic_uuid, "OHOH"))
