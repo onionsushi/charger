187b88844e1690067efd67c8e58bbbba9975b686
onionsushi <abelismyid@hotmail.com>
2023-08-19 00:16:01 -0400

    added bluetooth info

--- signal433.py
+++ signal433.py
@@ -17,3 +17,13 @@ def sendLockSignal():
     # Clean up GPIO resources
     GPIO.cleanup()
     print("Done")
+
+'''
+BLEService myService("a5d3a518-3d69-11ee-be56-0242ac120002");
+BLEIntCharacteristic myCharacteristic("bd613920-3d64-11ee-be56-0242ac120002", BLERead | BLEBroadcast);
+
+// Advertising parameters should have a global scope. Do NOT define them in 'setup' or in 'loop'
+const uint8_t manufactData[4] = {0x01, 0x02, 0x03, 0x04};
+const uint8_t serviceData[3] = {0x00, 0x01, 0x02};
+// address is c0:49:ef:92:5e:66
+'''
